0b53d524815b753c57c8c588be0bf940437802af
BlueBuck <gauravquest00@gmail.com>
2026-05-11 12:33:25 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const uri = process.env.MONGODB_URI || process.env.MONGO_URI;
+    await mongoose.connect(uri);
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("DB Error:", error.message);
